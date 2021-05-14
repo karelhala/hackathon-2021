ecafae45cb95979d1901109661de85ec34ce83dc
Karel Hala <khala@redhat.com>
2021-05-14 17:18:30 +0200

    Use PF react tokens to style views

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import global_BackgroundColor_100 from '@patternfly/react-tokens/dist/js/global_BackgroundColor_100';
+import global_Color_100 from '@patternfly/react-tokens/dist/js/global_palette_black_100';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1 }}>
+        <Text style={styles.text}>Header</Text>
+      </View>
+      <View style={{ flex: 10 }}>
+        <Text style={styles.text}>Content</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: global_BackgroundColor_100.value
   },
+  text: {
+    color: global_Color_100.value
+  }
 });
