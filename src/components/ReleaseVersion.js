f6fffebcd8b49850a7da2fdc20c46bdd41a6405c
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2021-05-20 21:39:57 +0200

    Include style to ReleaseVersion
    Co-authored-by: Priscila Gutierres <priscila.gutierres@gmail.com>

--- src/components/ReleaseVersion.js
+++ src/components/ReleaseVersion.js
@@ -3,11 +3,12 @@ import { StyleSheet, View, Text } from 'react-native';
 import { ThemeContext } from '../utils/contexts';
 
 const ReleaseVersion = ({system_profile}) => {
-    console.log(system_profile);
-    const { greyText } = useContext(ThemeContext);
+    const { text } = useContext(ThemeContext);
     if (Object.keys(system_profile).length != 0) {
         return <View>
-            <Text style={greyText}>{system_profile.operating_system.major}. {system_profile.operating_system.minor}</Text>
+            <View style={styles.label}>
+                <Text style={text}>{system_profile.operating_system.major}.{system_profile.operating_system.minor}</Text>
+            </View>
         </View>
     }
     else {
@@ -17,6 +18,10 @@ const ReleaseVersion = ({system_profile}) => {
     }   
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    label: {
+        backgroundColor: 'rgba(3, 3, 3, 0.32)'
+    }
+});
 
 export default ReleaseVersion;
