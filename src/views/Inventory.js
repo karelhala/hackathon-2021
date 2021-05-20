15c8457e74abeea8708b1007fe9dcf49bd3c0193
Ingrid Sena <10999758+fullsushidev@users.noreply.github.com>
2021-05-20 21:08:05 +0200

    Add pagination basic structure

--- src/views/Inventory.js
+++ src/views/Inventory.js
@@ -14,6 +14,7 @@ import global_Color_100 from '@patternfly/react-tokens/dist/js/global_palette_bl
 import darkColor from '@patternfly/react-tokens/dist/js/global_Color_dark_100';
 import Icon from '../components/Icon';
 import Remediate from '../game';
+import Pagination from '../components/Pagination';
 
 const Inventory = ({ navigation }) => {
   const { text, whiteText } = useContext(ThemeContext);
@@ -66,6 +67,7 @@ const Inventory = ({ navigation }) => {
                 </StackItem>
               </Stack>
           </Modal>
+          <Pagination />
           <Table
             header={[
               'Name',
